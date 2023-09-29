b8c05ce8d7f40a141b642a81c4555f0edd49a1f5
Svitlana Rybakova <rsv0011@gmail.com>
2023-09-29 11:08:24 +0200

    created the My Order Page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./components/home/Home";
-import Footer from "./components/layout/Footer";
-import Header from "./components/layout/Header";
-import Contact from "./components/contact/Contact";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/home/Home';
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Contact from './components/contact/Contact';
 import Cart from './components/cart/Cart';
+import Shipping from './components/cart/Shipping';
 
-// Add module imports for Shipping
+import Login from './components/login/Login';
+import Profile from './components/profile/Profile';
+import MyOrders from './components/myOrders/MyOrders';
 
-import Login from "./components/login/Login";
-import Profile from "./components/profile/Profile";
-// Add module imports for MY ORDERS
+import OrderDetails from './components/myOrders/OrderDetails';
+import About from './components/about/About';
 
-import OrderDetails from "./components/myOrders/OrderDetails";
-import About from "./components/about/About";
-
-
-import "./styles/app.scss";
-import "./styles/header.scss";
-import "./styles/home.scss";
-import "./styles/founder.scss";
-import "./styles/menu.scss";
-import "./styles/footer.scss";
-import "./styles/contact.scss";
-import "./styles/cart.scss";
-import "./styles/shipping.scss";
-import "./styles/login.scss";
-import "./styles/profile.scss";
-import "./styles/table.scss";
-import "./styles/orderDetails.scss";
-import "./styles/about.scss";
+import './styles/app.scss';
+import './styles/header.scss';
+import './styles/home.scss';
+import './styles/founder.scss';
+import './styles/menu.scss';
+import './styles/footer.scss';
+import './styles/contact.scss';
+import './styles/cart.scss';
+import './styles/shipping.scss';
+import './styles/login.scss';
+import './styles/profile.scss';
+import './styles/table.scss';
+import './styles/orderDetails.scss';
+import './styles/about.scss';
 
 function App() {
   return (
@@ -39,11 +37,11 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/about' element={<About />} />
         <Route path='/cart' element={<Cart />} />
-        
-        // Add the Route for SHIPPING
+        <Route path='/shipping' element={<Shipping />} />
+        <Route path='/shipping' element={<Shipping />} />
         <Route path='/login' element={<Login />} />
         <Route path='/me' element={<Profile />} />
-        // Add the Route for MY ORDERS
+        <Route path='/myorders' element={<MyOrders />} />
         <Route path='/order/:id' element={<OrderDetails />} />
       </Routes>
 
